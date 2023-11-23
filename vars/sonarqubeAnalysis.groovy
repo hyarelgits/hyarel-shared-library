c5e87d74ddd664842458d17317261cbae8b42387
hyarelgits <hyarel78@gmail.com>
2023-11-23 11:53:34 +0530

    forteen

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
-    withSonarQubeEnv('sonar-scanner') {
+    withSonarQubeEnv('sonar-server') {
         sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Youtube1 -Dsonar.projectKey=Youtube1 '''
     }
 }
